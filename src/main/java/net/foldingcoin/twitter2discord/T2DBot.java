2050388dd6c6b18dd6620c05754f85b74b1fc023
Jared <jaredlll08@gmail.com>
2018-06-19 18:06:47 +0200

    Started work on the twitter handler

--- src/main/java/net/foldingcoin/twitter2discord/T2DBot.java
+++ src/main/java/net/foldingcoin/twitter2discord/T2DBot.java
@@ -1,6 +1,8 @@
 package net.foldingcoin.twitter2discord;
 
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import net.darkhax.botbase.BotBase;
 import net.darkhax.botbase.commands.ManagerCommands;
@@ -10,6 +12,7 @@ import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IRole;
 import sx.blah.discord.handle.obj.IUser;
+import twitter4j.*;
 
 public class T2DBot extends BotBase {
     
@@ -28,7 +31,7 @@ public class T2DBot extends BotBase {
     
     @Override
     public void registerCommands(ManagerCommands handler) {
-        handler.registerCommand("link" , new CommandChannel());
+        handler.registerCommand("link", new CommandChannel());
     }
     
     @Override
@@ -40,17 +43,45 @@ public class T2DBot extends BotBase {
     @Override
     public void onSucessfulLogin(IDiscordClient instance) {
         
-        this.timer.scheduleRepeating(0, TimeUnit.HOURS.toMillis(6), FLDCStats::reload);
+        
+        this.timer.scheduleRepeating(0, TimeUnit.MINUTES.toMillis(1), T2DBot::checkTweets);
         
         // Guild Specific init
         this.roleAdmin = instance.getRoleByID(405483553904656386L);
         this.roleTeamFLDC = instance.getRoleByID(379170648208965633L);
     }
     
+    private static LinkedList<Long> statusList = new LinkedList<>();
     
-    
-    public static void checkTweets(){
-    
+    public static void checkTweets() {
+        try {
+            Twitter twitter = TwitterFactory.getSingleton();
+            List<Status> statuses = twitter.getUserTimeline("Jaredlll08");
+            
+            System.out.println("Showing home timeline.");
+            if(statusList.isEmpty()) {
+                for(Status status : statuses) {
+                    statusList.add(status.getId());
+                }
+                return;
+            }
+            LinkedList<Long> list = new LinkedList<>(statusList);
+            for(Status status : statuses) {
+                list.remove(status.getId());
+            }
+            for(Status status : statuses) {
+                if(list.contains(status.getId())) {
+                    System.out.println(status);
+                    if(status.getInReplyToScreenName() == null || status.isRetweetedByMe()) {
+                        System.out.println(status.getUser().getName() + ":" + status.getText());
+                    }
+                    statusList.add(status.getId());
+                }
+                //
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
     
     
